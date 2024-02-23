afa008243ddf0b8f7de505949b827ec591d334bd
Siroring <whiteloli99@gmail.com>
2024-02-23 19:43:47 +0900

    add OpAdd

--- vm/vm.go
+++ vm/vm.go
@@ -40,13 +40,21 @@ func (vm *VM) Run() error {
 
     switch op {
       case code.OpConstant:
-      constIndex := code.ReadUint16(vm.instructions[ip+1:])
-      ip += 2
+        constIndex := code.ReadUint16(vm.instructions[ip+1:])
+        ip += 2
 
-      err := vm.push(vm.constants[constIndex])
-      if err != nil {
-        return err
-      }
+        err := vm.push(vm.constants[constIndex])
+        if err != nil {
+          return err
+        }
+      case code.OpAdd:
+        right := vm.pop()
+        left := vm.pop()
+        leftValue := left.(*object.Integer).Value
+        rightValue := right.(*object.Integer).Value
+
+        result := leftValue + rightValue
+        vm.push(&object.Integer{Value: result})
     }
   }
   return nil
@@ -61,4 +69,10 @@ func (vm *VM) push(o object.Object) error {
   vm.sp++
 
   return nil
+}
+
+func (vm *VM) pop() object.Object {
+  o := vm.stack[vm.sp-1]
+  vm.sp--
+  return o
 }
