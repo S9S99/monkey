5e6aab15ae63ccbc65d8ef0a499e7614145f95b4
Siroring <whiteloli99@gmail.com>
2024-02-25 17:39:10 +0900

    add hash

--- code/code.go
+++ code/code.go
@@ -31,6 +31,7 @@ var definitions = map[Opcode]*Definition{
   OpGetGlobal:      {"OpGetGlobal", []int{2}},
   OpSetGlobal:      {"OpSetGlobal", []int{2}},
   OpArray:          {"OpArray", []int{2}},
+  OpHash:           {"OpHash", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -66,6 +67,7 @@ const (
   OpGetGlobal
   OpSetGlobal
   OpArray
+  OpHash
 )
 
 func Make(op Opcode, operands ...int) []byte {
