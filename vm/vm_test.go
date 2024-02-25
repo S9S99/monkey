092929f88c47217ace0c0000003d2e7762303287
Siroring <whiteloli99@gmail.com>
2024-02-25 16:46:45 +0900

    add string

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -93,6 +93,16 @@ func TestGlobalLetStatements(t *testing.T) {
   runVmTests(t, tests)
 }
 
+func TestStringExpressions(t *testing.T) {
+  tests := []vmTestCase{
+    {`"monkey"`, "monkey"},
+    {`"mon" + "key"`, "monkey"},
+    {`"mon" + "key" + "banana"`, "monkeybanana"},
+  }
+
+  runVmTests(t, tests)
+}
+
 type vmTestCase struct {
   input    string
   expected interface{}
@@ -134,6 +144,11 @@ func testExpectedObject(
     err := testIntegerObject(int64(expected), actual)
     if err != nil {
       t.Errorf("testIntegerObject failed: %s", err)
+    }
+  case string:
+    err := testStringObject(expected, actual)
+    if err != nil {
+      t.Errorf("testStringObject failed: %s", err)
     }
   case bool:
     err := testBooleanObject(bool(expected), actual)
@@ -177,6 +192,21 @@ func testIntegerObject(expected int64, actual object.Object) error {
 
   if result.Value != expected {
     return fmt.Errorf("object has wrong value. got=%d, want=%d",
+      result.Value, expected)
+  }
+
+  return nil
+}
+
+func testStringObject(expected string, actual object.Object) error {
+  result, ok := actual.(*object.String)
+  if !ok {
+    return fmt.Errorf("object is not String. got=%T (%+v)",
+      actual, actual)
+  }
+
+  if result.Value != expected {
+    return fmt.Errorf("object has wrong value. got=%q, want=%q",
       result.Value, expected)
   }
 
