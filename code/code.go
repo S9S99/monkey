a8cce8d27f38d41db2027040345dcf839911e9ce
Siroring <whiteloli99@gmail.com>
2024-02-25 17:06:43 +0900

    add string

--- code/code.go
+++ code/code.go
@@ -30,6 +30,7 @@ var definitions = map[Opcode]*Definition{
   OpNull:           {"OpNull", []int{}},
   OpGetGlobal:      {"OpGetGlobal", []int{2}},
   OpSetGlobal:      {"OpSetGlobal", []int{2}},
+  OpArray:          {"OpArray", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -64,6 +65,7 @@ const (
   OpNull
   OpGetGlobal
   OpSetGlobal
+  OpArray
 )
 
 func Make(op Opcode, operands ...int) []byte {
--- vm/vm_test.go
+++ vm/vm_test.go
@@ -103,6 +103,16 @@ func TestStringExpressions(t *testing.T) {
   runVmTests(t, tests)
 }
 
+func TestArrayLiterals(t *testing.T) {
+  tests := []vmTestCase{
+    {"[]", []int{}},
+    {"[1, 2, 3]", []int{1, 2, 3}},
+    {"[1 + 2, 3 * 4, 5 + 6]", []int{3, 12, 11}},
+  }
+
+  runVmTests(t, tests)
+}
+
 type vmTestCase struct {
   input    string
   expected interface{}
@@ -154,6 +164,25 @@ func testExpectedObject(
     err := testBooleanObject(bool(expected), actual)
     if err != nil {
       t.Errorf("testBooleanObject failed: %s", err)
+    }
+  case []int:
+    array, ok := actual.(*object.Array)
+    if !ok {
+      t.Errorf("object not Array: %T (%+v)", actual, actual)
+      return
+    }
+
+    if len(array.Elements) != len(expected) {
+      t.Errorf("wrong num of elements. want=%d, got=%d",
+        len(expected), len(array.Elements))
+      return
+    }
+
+    for i, expectedElem := range expected {
+      err := testIntegerObject(int64(expectedElem), array.Elements[i])
+      if err != nil {
+        t.Errorf("testIntegerObject failed: %s", err)
+      }
     }
   case *object.Null:
     if actual != Null {
