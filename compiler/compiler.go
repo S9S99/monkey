f4eb9a42c275b09c81d296281a4a9222352f83bc
Siroring <whiteloli99@gmail.com>
2024-02-25 17:53:46 +0900

    add index

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -95,6 +95,17 @@ func (c *Compiler) Compile(node ast.Node) error {
       }
     }
     c.emit(code.OpHash, len(node.Pairs)*2)
+  case *ast.IndexExpression:
+    err := c.Compile(node.Left)
+    if err != nil {
+      return err
+    }
+
+    err = c.Compile(node.Index)
+    if err != nil {
+      return err
+    }
+    c.emit(code.OpIndex)
   case *ast.Identifier:
     symbol, ok := c.symbolTable.Resolve(node.Value)
     if !ok {
