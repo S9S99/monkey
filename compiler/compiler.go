29dcdb64c6ec1f21e4055f0b9ca46d13efdf5c47
Siroring <whiteloli99@gmail.com>
2024-03-01 00:58:36 +0900

    add local binding compile

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -102,7 +102,11 @@ func (c *Compiler) Compile(node ast.Node) error {
       return err
     }
     symbol := c.symbolTable.Define(node.Name.Value)
-    c.emit(code.OpSetGlobal, symbol.Index)
+    if symbol.Scope == GlobalScope {
+      c.emit(code.OpSetGlobal, symbol.Index)
+    } else {
+      c.emit(code.OpSetLocal, symbol.Index)
+    }
 
   case *ast.ArrayLiteral:
     for _, el := range node.Elements {
@@ -147,7 +151,11 @@ func (c *Compiler) Compile(node ast.Node) error {
     if !ok {
       return fmt.Errorf("undefined variable %s", node.Value)
     }
-    c.emit(code.OpGetGlobal, symbol.Index)
+    if symbol.Scope == GlobalScope {
+      c.emit(code.OpGetGlobal, symbol.Index)
+    } else {
+      c.emit(code.OpGetLocal, symbol.Index)
+    }
 
   case *ast.Boolean:
     if node.Value {
@@ -364,6 +372,8 @@ func (c *Compiler) enterScope() {
   }
   c.scopes = append(c.scopes, scope)
   c.scopeIndex++
+
+  c.symbolTable = NewEnclosedSymbolTable(c.symbolTable)
 }
 
 func (c *Compiler) leaveScope() code.Instructions {
@@ -371,6 +381,8 @@ func (c *Compiler) leaveScope() code.Instructions {
 
   c.scopes = c.scopes[:len(c.scopes)-1]
   c.scopeIndex--
+
+  c.symbolTable = c.symbolTable.Outer
 
   return instructions
 }
